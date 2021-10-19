1ab1c37a9eb04a8ab5176714e421eb5da73343e1
Matilda Wintence <matildawintence@Matildas-MacBook-Pro.local>
2021-10-19 13:48:38 +0200

    Initial commit

---
+++ src/com/Wintence/Main.java
@@ -0,0 +1,10 @@
+package com.Wintence;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String s = "Hej";
+        System.out.println(s);
+    }
+}
