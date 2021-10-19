59a3bca820b0900cfa4e834322634261fa8d21a2
matildawintence <matilda@wintence.com>
2021-10-19 14:03:12 +0200

    La till systemout

--- src/com/Wintence/Main.java
+++ src/com/Wintence/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String s = "Hej";
-        System.out.println(s);
+        String y = "Då";
+        System.out.println(s + y);
     }
 }
